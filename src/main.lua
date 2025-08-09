728a1c2a8882252500d6a2381670c9f20eaa60dc
Gabriel James Benedict <benok.loslos@gmail.com>
2025-08-09 15:26:48 +0800

    refactor: rename variables for clarity in drawing function

--- src/main.lua
+++ src/main.lua
@@ -9,15 +9,16 @@ function love.update(dt)
 end
 
 function love.draw()
+
     love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("line", 350, 150, 1200, 750)
 
     love.graphics.setColor(157/255, 95/255, 70/255)
     love.graphics.rectangle("fill", 400, 200, 850, 650)
 
-    local r = 80
-    local cx = 300 + 1200 - r 
-    local cy = 200 + r        
+    local radius = 80
+    local circleX = 300 + 1200 - r 
+    local circleY = 200 + r        
 
     love.graphics.setColor(255, 0, 0) 
     love.graphics.circle("fill", cx, cy, r)
