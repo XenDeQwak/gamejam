31b591f43cfe5c1d84832edbccc85b46575549e2
XenDQ <xeanxyrone@gmail.com>
2025-08-09 15:45:10 +0800

    merge

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ local scale, offsetX, offsetY
 function love.load()
     love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
     updateScale(love.graphics.getDimensions())
+    lever = {}
+    lever.x = 1420
+    lever.y = 280
 end
 
 function love.resize(w, h)
@@ -23,6 +26,12 @@ function love.draw()
     love.graphics.pop()
 end
 
+function love.update(dt)
+     if love.mouse.isDown(1) then
+        lever.y = lever.y + 5
+     end
+end
+
 function createScreen()
     love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("line", 350, 150, 1200, 750)
@@ -30,10 +39,13 @@ function createScreen()
     love.graphics.setColor(157/255, 95/255, 70/255)
     love.graphics.rectangle("fill", 400, 200, 850, 650)
 
-    local r = 80
-    local cx = 300 + 1200 - r 
-    local cy = 200 + r        
+    local r = 80       
 
     love.graphics.setColor(255, 0, 0) 
-    love.graphics.circle("fill", cx, cy, r)
+    love.graphics.circle("fill", lever.x, lever.y, r)
+end
+
+function getMouse()
+    
+
 end
