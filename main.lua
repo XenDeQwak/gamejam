41f780cbfd2b196be7b653bbc631c4ca05bbc338
XenDQ <xeanxyrone@gmail.com>
2025-08-09 11:04:27 +0800

    initial commit + test of love2d

---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+    player = {}
+    pc = {}
+    love.window.setMode(1920, 1080, {resizable = true})
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.rectangle("line", 350, 150, 1200, 750)
+end
