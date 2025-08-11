2cc37c16dda63dd0cae87a098536580e651f7728
Gabriel James Benedict <benok.loslos@gmail.com>
2025-08-11 23:49:52 +0800

    refactor: return the decrease amount

--- src/slotmachine.lua
+++ src/slotmachine.lua
@@ -1,8 +1,8 @@
 local slotmachine = {}
 
-slotmachine.initialWinningChance = 0.5
+slotmachine.initialWinningChance = 0.60
 slotmachine.currentWinningChance = slotmachine.initialWinningChance
-slotmachine.decreaseAmount = {0.0}
+slotmachine.decreaseAmount = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10}
 slotmachine.decreaseChance = 0.10
 
 local symbols = {
