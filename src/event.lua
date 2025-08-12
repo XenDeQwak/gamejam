fe73aac89c3bee9d1f6ae125ed2f2de834cf8770
Centhor <vincenthizon266@gmail.com>
2025-08-12 11:25:37 +0800

    event sequence plotted

--- src/event.lua
+++ src/event.lua
@@ -1,6 +1,6 @@
 local notifSprite
 local event = {
-    isBroke=false,
+    isBroke=true,
     hasDebt=false,
     hasFamily=true,
     familyAnger=0,
@@ -16,10 +16,6 @@ function event.load()
 end
 
 function event.draw()
-    love.graphics.draw(notifSprite,575,300,0,0.4,0.4)
-
-    -- love.graphics.setColor(color.setRGBA(255,0,0))
-    -- love.graphics.rectangle("fill",1000,,100,1200)
    
     --this is just a grey overlay bg thing, only way i could get it to work for some reason
     love.graphics.push()
@@ -31,24 +27,12 @@ function event.draw()
     love.graphics.setColor(1,1,1,1)
     love.graphics.draw(notifSprite,575,350,0,0.4,0.4)
 
-    love.graphics.setColor(color.setRGBA(255,0,0))
-    -- love.graphics.rectangle("fill",1000,,100,1200)
-
     love.graphics.setColor(color.setRGBA(0,0,0))
-    
-    local boxheight = 352
-    local boxStart = 300
-    local messageY = (boxheight*0.5)+boxStart
+    local messageHeight = 352
+    local messageStart = 300
+    local messageY = (messageHeight*0.5)+messageStart
     local maxWidth=350
     love.graphics.printf(event.message,900,messageY,maxWidth,"center")
-
-end
-
-function outOfMoney()
-    print("OUT OF MONEY")
-    event.message="OUT OF MONEY"
-    event.popupTimer=3
-    -- Popup, GO TO MAFIA or SELL HOUSE
 
 end
 
@@ -56,30 +40,95 @@ local familyActions ={
     [0] = function()
     notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
     event.message=("Dad, let's go to the park!")
+    --if yes, familyAnger = 0
+    --if no, familyAnger++
     end,
 
     [1] = function ()
+    notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
+    event.message=("Honey, we didn't get to celebrate Olivia's birthday. Make it up to her.")
+
+    --if "Plan Party"
+        --if money<1000
+            --love.graphics.print(Not Enough Money)
+        --else
+            --familyAnger = 0
+            --money-=1000
+    --if "No"
+        --familyAnger++
 
     end,
 
     [2] = function ()
+    notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
+    event.message=("Dad, is that more important than me?")
+
+    --if "Yes" 
+        --familyAnger++
+    --if "Buy Gifts"
+        --if money<3000,
+            --love.graphics.print(Not Enough Money)
+        --else
+            --familyAnger = 0
+            --money -= -3000
 
     end,
 
     [3] = function ()
+    notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
+    event.message=("This isn't good for Olivia, I am thinking about leaving...")
+
+    --if "whatever..", 
+        --hasFamily = false
+        --notifSprite = divorce popup
+        --event.message=("Your family has left")
+
+    --if "Make-Up with Family" 
+        --if money<5000, 
+            --love.graphics.print(Not Enough Money)
+        --else
+            --money -= 5000
+            --familyAnger = 2
 
     end
 }
 
-function mafiaDebt()
+local function mafiaDebt()
     print("MAFIA DEBT")
-    event.message="MAFIA DEBT"
-    event.popupTimer=3
-    -- if choice no, cooldown 3
-    -- if choice yes, money== money-debt, hasDebt=false
+    notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
+    event.message=("We've come to collect")
+
+    -- if "Give me time"
+        --if mafiaWait == 3
+            --gunCock, death
+        --mafiaWait++
+    -- elseif choice "Pay"
+        --money = money-debt*1.1
+        --hasDebt=false
+
 end 
 
-local allEvents = {
+local function outOfMoney()
+    print("OUT OF MONEY")
+    notifSprite = love.graphics.newImage("assets/ui/daughter1.png")
+    event.message=("You are out of money")
+
+    -- Popup, GO TO MAFIA or SELL HOUSE
+    --if SELL HOUSE
+        --isBroke = false
+        --money+=10000
+        --hasFamily = false, 
+        --notifSprite = divorce popup
+        --event.message=("Your family has left")
+    --elseif MAFIA
+        --isBroke = false
+        --hasDebt = true
+        --money+=3000,
+        --notifSprite = mafia popup
+        --event.message=(We will come to collect")
+end
+
+local allEvents = { --this is prioritized based on sequence (broke will occur first if available)
     {
         name = "Broke",
         condition = function() return event.isBroke end,
@@ -87,7 +136,6 @@ local allEvents = {
         cooldown = 0
     },
     {
-        -- TODO: Say NO, cooldown == 3
         name = "Debt",
         condition = function() return event.hasDebt end,
         action = mafiaDebt,
@@ -96,33 +144,27 @@ local allEvents = {
     {
         name = "Family",
         condition = function() return event.hasFamily end,
-        action = familyActions[event.familyAnger],
+        action = function() familyActions[event.familyAnger]() end,
         cooldown=0
     }
 }
 
-function availableEvents()
-    local available = {}
-    for index,event in ipairs(allEvents) do
-        if event.cooldown > 0 then
-            event.cooldown = event.cooldown - 1
-        elseif event.condition() then
-            table.insert(available, event)
+function event.nextEvent()
+    local triggered=nil
+
+    for index,events in ipairs(allEvents) do
+        print(events.name)
+    end
+
+    for index,events in ipairs(allEvents) do
+        if events.cooldown > 0 then
+            events.cooldown = events.cooldown - 1
+        elseif not triggered and events.condition() then
+            events.action()
+            events.cooldown = 3
+            triggered=true
         end
     end
-    return available
-end
-
-function event.nextEvent()
-    for index,event in ipairs(availableEvents()) do
-        print(event.name)
-    end
-    local available = availableEvents()
-    if #available == 0 then
-        return nil
-    end
-    local choice = available[math.random(#available)]
-    choice.action()
 end
 
 return event 
